d913bc639089776a560d121eb80c7b924a984648
Software Laptop 1 <software-laptop-1@users.noreply.github.com>
2019-01-26 18:23:25 +0100

    Initial Cart Subsystem that doesn't work yet.

---
+++ src/main/java/ch/fridolinsrobotik/sensors/utils/EncoderConverter.java
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package ch.fridolinsrobotik.sensors.utils;
+
+/**
+ * Add your docs here.
+ */
+public class EncoderConverter {
+
+    private double distancePerPulse = 1;
+
+    public void setDistancePerPulse(double distance) {
+        this.distancePerPulse = distance;
+    }
+
+    public double getDistancePerPulse() {
+        return this.distancePerPulse;
+    }
+
+    public double getDistance(double encoderPulses) {
+        return encoderPulses * this.distancePerPulse;
+    }
+    
+}
