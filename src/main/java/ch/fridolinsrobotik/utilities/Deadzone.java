f482af193b4d22aecaf88b29e00981652ba3531c
Florian Wiederkehr <florian@widi.ch>
2019-02-02 21:13:46 +0100

    Deadzone for Joysticks
    Tims code for a Joystick Deadzone

---
+++ src/main/java/ch/fridolinsrobotik/utilities/Deadzone.java
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package ch.fridolinsrobotik.utilities;
+
+/**
+ * Add your docs here.
+ */
+public class Deadzone {
+
+    public static double getAxis(double inputAxis, double inputDeadzone) {
+        double outputAxis = 0;
+
+        // Tests if the Value is positive or negative
+        if (inputAxis > inputDeadzone) {
+            outputAxis = Algorithms.scale(inputAxis, inputDeadzone, 1, 0, 1);
+        } else if (inputAxis < -inputDeadzone) {
+            outputAxis = Algorithms.scale(inputAxis, -inputDeadzone, -1, 0, -1);
+        }
+        return outputAxis;
+    }
+
+}
